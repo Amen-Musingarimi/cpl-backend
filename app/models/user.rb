3a1123e40627c9298d87e21656a8d8f794b59652
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-10-03 10:43:02 +0200

    Add system admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,18 @@ class User < ApplicationRecord
   has_one :league_admin
   has_one :team_admin
   has_one :team_member
-  
+
   has_secure_password
+
+  enum team_role: {
+    system_admin: 'system_admin',
+    league_admin: 'league_admin',
+    team_admin: 'team_admin',
+    team_member: 'team_member'
+  }
 
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+super_admin = User.create!(
+  name: 'Amen',
+  surname: 'Musingarimi',
+  email: 'takudzwamusinga@gmail.com',
+  date_of_birth: '11-02-1995',
+  password: 'Takudzwa95!',
+  national_id_number: '45-209926Y22',
+  team: 'Ligi Mabelreign',
+  phone_number: '0712539755',
+  team_role: 'system_admin'
+)
+
+puts "Super Admin created with email: #{super_admin.email}"
+
